4e3c3fe6158ebd2a6e6a51f47bad6682a9185494
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2023-04-12 11:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 //complete this code
 class Animal {
-	constructor(species) {
-	    this._species = species;
-	}
+  constructor(species) {
+    this._species = species;
+  }
 
   get species() {
     return this._species;
   }
 
   makeSound() {
-    console.log("The animal makes a sound");
+    return "The animal makes a sound";
   }
 }
 
 class Dog extends Animal {
-	bark() {
-	    console.log("woof");
-	}
+  makeSound() {
+    return "Woof";
+  }
+
+  bark() {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
+  makeSound() {
+    return "Meow";
+  }
+
   purr() {
     console.log("purr");
   }
